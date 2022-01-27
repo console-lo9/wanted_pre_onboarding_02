502c67255d31d62aa89317039169da2d052bdd43
박중섭 <>
2022-01-27 21:30:39 +0900

    Add: 기타 폴더에 감사카드 제공 컴포넌트 작성

--- src/components/Etc/ProvideCard.jsx
+++ src/components/Etc/ProvideCard.jsx
@@ -1,7 +1,18 @@
+import OnOffButton from "layout/OnOffButton";
 import React from "react";
+import styles from "./ProvideCard.module.css";
+
+const ID_5 = "id5";
 
 function ProvideCard() {
-  return <div>fd</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.name}>감사카드 제공</div>
+      <div className={styles.content}>
+        <OnOffButton id={ID_5} />
+      </div>
+    </div>
+  );
 }
 
 export default ProvideCard;
--- src/components/ProductBenefit/AccumulateMileage.jsx
+++ src/components/ProductBenefit/AccumulateMileage.jsx
@@ -2,14 +2,14 @@ import OnOffButton from "layout/OnOffButton";
 import React from "react";
 import styles from "./AccumulateMileage.module.css";
 
-const ID_2 = "id2";
+const ID_4 = "id4";
 
 function AccumulateMileage() {
   return (
     <div className={styles.container}>
       <div className={styles.name}>마일리지 적립</div>
       <div className={styles.content}>
-        <OnOffButton id={ID_2} />
+        <OnOffButton id={ID_4} />
       </div>
     </div>
   );
