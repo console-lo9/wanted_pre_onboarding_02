334b629dd4bc36011fdd3bb5a6a3b816f5cb8c79
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-28 14:58:38 +0900

    Add: 재사용가능한

--- src/components/ProductThumbnail/AddImage.jsx
+++ src/components/ProductThumbnail/AddImage.jsx
@@ -2,7 +2,7 @@ import SectionBody from "layout/Section/SectionBody";
 import SectionBodyContent from "layout/Section/SectionBodyContent";
 
 import UploadInput from "layout/Inputs/UploadInput";
-
+import Button from "layout/Button";
 import styles from "./AddImage.module.css";
 
 const AddImage = () => {
@@ -10,6 +10,7 @@ const AddImage = () => {
     <SectionBody className={styles.addImage}>
       <SectionBodyContent>
         <UploadInput />
+        <Button tag="xBadge">×</Button>
       </SectionBodyContent>
     </SectionBody>
   );
