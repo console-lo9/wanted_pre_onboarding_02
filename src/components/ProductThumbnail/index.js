c62a416857a5acab971521069b77b45aa336e74e
Ubermensch0608 <melancholyee28@gmail.com>
2022-01-29 15:34:16 +0900

    Add: 구매자 추천 이미지 섹션 추가

--- src/components/ProductThumbnail/index.js
+++ src/components/ProductThumbnail/index.js
@@ -1,13 +1,20 @@
 import SectionHeader from "layout/Section/SectionHeader";
 import SectionWrapper from "layout/Section/SectionWrapper";
+import { Fragment } from "react";
 import AddImage from "./AddImage";
 
 const ProductThumbnail = () => {
   return (
-    <SectionWrapper>
-      <SectionHeader>상품 소개 이미지</SectionHeader>
-      <AddImage />
-    </SectionWrapper>
+    <Fragment>
+      <SectionWrapper>
+        <SectionHeader>상품 소개 이미지</SectionHeader>
+        <AddImage />
+      </SectionWrapper>
+      <SectionWrapper>
+        <SectionHeader>구매자 추천 이미지</SectionHeader>
+        <AddImage />
+      </SectionWrapper>
+    </Fragment>
   );
 };
 
